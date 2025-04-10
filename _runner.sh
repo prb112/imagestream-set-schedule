8eb5c5ab22dc1a0a1d8ba9a97eeaefcbfb035cea
Paul Bastide <pbastide@us.ibm.com>
2025-04-10 15:33:09 -0400

    test: initial test cases
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

---
+++ _runner.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "START TEST: $1 [$2]"
+RESULT=$(cat $2 | \
+    jq -r '
+    [.items[] | 
+        ( .spec.tags[]?.from.kind != "ImageStreamTag"
+            and
+            (
+                [
+                    .spec.tags[]? |
+                        (.importPolicy.scheduled == null or .importPolicy.scheduled == false)
+                ] | any
+            )
+        )
+        and
+        (
+            (.metadata.labels == null)
+            or
+            (.metadata.labels."samples.operator.openshift.io/managed" == null)
+            or
+            (.metadata.labels."samples.operator.openshift.io/managed" != "true")
+        )
+        and
+        (
+            (.metadata.ownerReferences == null)
+            or
+            (
+                (.metadata.ownerReferences[].kind != null)
+                and
+                (.metadata.ownerReferences[].kind != "ClusterVersion")
+            )
+        )] | any') 
+
+echo "GOT: ${RESULT}"
+echo "EXP: ${3}"
+
+echo "END"
+echo ""
---
+++ _tests.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+./_runner.sh "Kind: ImageStreamTag" 0_imagestreamtag.json false
+./_runner.sh "Kind: DockerImage" 1_dockerimage.json true
+./_runner.sh "empty tags" 2_empty_tags.json false
+./_runner.sh "Docker Image Scheduled False" 3_dockerimage_scheduled_false.json true
+./_runner.sh "Docker Image Scheduled nil" 3_dockerimage_scheduled_nil.json true
+./_runner.sh "Docker Image Scheduled true" 3_dockerimage_scheduled_true.json false
+
+./_runner.sh "Docker Image Scheduled multi first true" 3_dockerimage_scheduled_multi_first.json true
+./_runner.sh "Docker Image Scheduled multi first none" 3_dockerimage_scheduled_multi_none.json true
+./_runner.sh "Docker Image Scheduled multi all true"  3_dockerimage_scheduled_multi_all.json false
+./_runner.sh "Docker Image Scheduled multi all false"  3_dockerimage_scheduled_multi_false.json true
+./_runner.sh "Docker Image Scheduled multi last true" 3_dockerimage_scheduled_multi_last.json true
+
+./_runner.sh "Sample is false" 4_samp_false.json true
+./_runner.sh "Labels don't exist" 4_samp_labels_empty.json true
+./_runner.sh "Sample Nil" 4_samp_nil.json true
+./_runner.sh "Samp other" 4_samp_other.json true
+./_runner.sh "Samp True" 4_samp_true.json false
+
+
+./_runner.sh "OwnerReferences - Empty" 5_or_empty.json true
+./_runner.sh "OwnerReferences - Kind Alternative" 5_or_kind_alt.json true
+./_runner.sh "OwnerReferences - Kind Exists ClusterVersion" 5_or_kind_cluster_version.json false
+./_runner.sh "OwnerReferences - Kind not exists" 5_or_kind_not_exists.json true
+./_runner.sh "OwnerReferences - Kind ClusterVersion Second" 5_or_kind_second.json false
+./_runner.sh "OwnerReferences - None" 5_or_none.json true
